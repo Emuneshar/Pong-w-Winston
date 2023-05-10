599c0f729b20d1ed5017acf6f305cb9b5bab23dd
Emuneshar <60401349+Emuneshar@users.noreply.github.com>
2023-05-10 20:02:12 +0000

    Added shape for our paddles

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ window = turtle.Screen()
 window.title("Pong")
 window.bgcolor("black")
 window.setup(width=800, height=600)
-window.tracer(0)
+#window.tracer(0)
 
 score_left = 0
 score_right = 0
@@ -14,12 +14,13 @@ ball = turtle.Turtle()
 ball.penup()
 ball.shape("circle")
 ball.color("blue")
-ball.shapesize(0.5, 0.5)
+ball.goto(0,0)
 
 paddleOne = turtle.Turtle()
 paddleOne.penup()
 paddleOne.speed(0)
 paddleOne.shape("square")
+paddleOne.shapesize( )
 
 
 paddleTwo = turtle.Turtle()

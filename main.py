01cecb5fd1d76bff52c4f44de8b154cec3033820
Emuneshar <60401349+Emuneshar@users.noreply.github.com>
2023-05-10 19:52:58 +0000

    Added code to setup window

---
+++ main.py
@@ -0,0 +1,13 @@
+import turtle
+import os 
+
+window = turtle.Screen()
+window.title("Pong")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+score_left = 0
+score_right = 0
+
+
